14ca2478df67c6b3432ee3268fb7587584d4fa23
ianwambaire <ian.wambaire@strathmore.edu>
2025-06-08 21:35:37 +0300

    Higher Order Function

--- first.dart
+++ first.dart
@@ -1,5 +1,14 @@
 void main() {
-  var itemPrices = [5.0,10.0, 20.0, 30.0, 40.0, 50.0];
-  var filteredItems = itemPrices.where((price) => price >= 10).toList();
+  List<double> itemPrices = [5.0, 10.0, 20.0, 30.0, 40.0, 50.0];
+  List<double> filteredItems =
+      itemPrices.where((price) => price >= 10).toList();
   print("Filtered items $filteredItems");
+  List<double> dicountedItems =
+      applyDiscount(filteredItems, (price) => price * 0.9);
+  print("Discounted items $dicountedItems");
+}
+
+List<double> applyDiscount(
+    List<double> items, double Function(double) discountFunction) {
+  return items.map(discountFunction).toList();
 }
